eacabbd22c44c36aa3b93123b26adcb2082cbe0a
Aria Bennett <twoplustwo@gmail.com>
2012-09-18 17:59:22 -0700

    First Commit

---
+++ Project Euler Problem 15/Project Euler Problem 15.cpp
@@ -0,0 +1,18 @@
+// Project Euler Problem 15.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void pause()
+{
+	cin.clear();
+	cin.get();
+}
+
+int main()
+{
+	return 0;
+}
+
